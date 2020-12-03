0afce9bbb7394820b438645d6e2033c6f1a2afe6
Ricardo Villada <ricardovillada@hotmail.com>
2020-12-03 16:53:19 -0500

    Query para realizar una nueva publicacion

--- registrate.php
+++ registrate.php
@@ -51,7 +51,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 
         if($errores == ''){
 
-           $statement = $conexion->prepare('INSERT INTO user ( id, order_id, user_type, name, last_name, email, genre, nick_name, password) 
+           $statement = $conexion->prepare('INSERT INTO user ( id, post_id, user_type, name, last_name, email, genre, nick_name, password) 
                                             VALUES (null, null, null, :name, :last_name,  :email,:genre, :nick_name, :password)');
 
            $statement->execute(array(
