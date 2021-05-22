362a952252937127dedfc9626ea38cc1405a57c8
RicardoAVS <ricardovillada@hotmail.com>
2021-05-22 09:29:37 -0500

    re-creating dev branch

--- admin/paypal_config.php
+++ admin/paypal_config.php
@@ -4,7 +4,6 @@ $itemName = 'Subscripcion';
 $itemNumber = 'MS123456'; 
 
 // Get Paypal web token
-
 $PAYPAL_CLIENT_ID = 'AWgDlDxJK5zmLxzqbCRSHEbjjgkoJCdS7DZ9UPO0wgpsrbnPqevHYYUmk2T3yr_p945aEMb996bu17xy';
 $PAYPAL_SECRET = 'EDi2oOdw_B0-uyutEa2tQl4bgj5gv757-se158QPdswzKz8KYSYs3wWTb0hVmCh1nwqoeXY0ODgBptaV';
 $curl = curl_init();
@@ -53,5 +52,28 @@ define('DB_NAME', 'tienda_online');
 // Change not required  
 define('PAYPAL_URL', "https://api-m.sandbox.paypal.com//v1/billing/plans");
 
+$curl = curl_init();
+curl_setopt_array($curl, array(
+CURLOPT_URL => "https://api.sandbox.paypal.com/v1/oauth2/token",
+CURLOPT_RETURNTRANSFER => true,
+CURLOPT_ENCODING => "",
+CURLOPT_MAXREDIRS => 10,
+CURLOPT_TIMEOUT => 30,
+CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+CURLOPT_CUSTOMREQUEST => "POST",
+CURLOPT_USERPWD => $PAYPAL_CLIENT_ID.":".$PAYPAL_SECRET,
+CURLOPT_POSTFIELDS => "grant_type=client_credentials",
+CURLOPT_HTTPHEADER => array(
+"Accept: application/json",
+"Accept-Language: en_US"
+),
+));
 
+$result= curl_exec($curl);
+
+$array=json_decode($result, true); 
+$token=$array['access_token'];
+
+echo "<pre>";
+print_r($array);
 ?>
